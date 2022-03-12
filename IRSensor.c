ed779ff3c9f00a58c49af9f0457c3cba900dd3dc
yq <yuanqichau@gmail.com>
2022-03-12 17:07:15 +0800

    add IRSensor

---
+++ IRSensor.c
@@ -0,0 +1,35 @@
+#pragma config(Sensor, in8,    topIRSensor,    sensorAnalog)
+#pragma config(Sensor, in1,    backIRSensor,   sensorAnalog)
+#pragma config(Sensor, in2,    rightIRSensor,  sensorAnalog)
+#pragma config(Sensor, in3,    leftIRSensor,   sensorAnalog)
+
+typedef struct{
+	int pin;
+	float factor;
+	float exponent;
+}IRSensor;
+
+void initializeSensor(IRSensor* sensor, int pin, float factor, float exponent){
+	sensor->pin = pin;
+	sensor->factor = factor;
+	sensor->exponent = exponent;
+}
+
+int getIRSensorReading(IRSensor* sensor){
+	float volt = (float)SensorValue[sensor->pin]/4096*5;
+	float distance = sensor->factor * pow(volt , sensor->exponent);
+	return distance;
+}
+
+
+task main()
+{
+	IRSensor frontSensor;
+	initializeSensor(&frontSensor, topIRSensor, 27.534, -1.207);
+
+	while (true) {
+		int reading = getIRSensorReading(frontSensor);
+		writeDebugStreamLine("Reading: %d", reading);
+	}
+
+}
