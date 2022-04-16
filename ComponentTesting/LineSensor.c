595d79e9985e455abfdae56192410c68680fd55e
RedyChee <cheesungyeng@gmail.com>
2022-04-16 15:40:45 +0800

    delete repetitive files

--- ComponentTesting/LineSensor.c
+++
@@ -1,9 +0,0 @@
-#pragma config(Sensor, dgtl11,  LF,         sensorDigitalIn)
-
-
-int val;
-task main(){
-	while(true){
-		val = SensorValue(LF);
-	}
-}
--- ComponentTesting/PowerSwitch.c
+++
@@ -1,9 +0,0 @@
-#pragma config(Sensor, dgtl1, Power_Switch, sensorDigitalIn)
-
-int val;
-
-task main(){
-	while(true){
-		val = SensorValue(Power_Switch);
-	}
-}
